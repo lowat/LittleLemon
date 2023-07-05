5c310302d2d0c0012ea2af175c85977218268c04
Logan Watkins <lw2014wa@gmail.com>
2023-07-05 15:13:54 -0500

    Add capstone code

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin 
 from django.urls import path 
-from . import views 
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
   
 urlpatterns = [ 
-    path('', views.index, name='index')
-
+    path('', views.index, name='index'),
+    path('menu/', views.MenuView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuView.as_view()),
+    path('menuitem/', views.MenuItemView.as_view()),
+    path('menuitem/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
